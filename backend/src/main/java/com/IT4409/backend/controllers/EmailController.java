d1e8cd67c65614b756187a6d0c321b6c37195804
20215459-phuongvv <phuong.vv215459@sis.hust.edu.vn>
2024-05-01 23:04:28 +0700

    Fix spring mail API

--- backend/src/main/java/com/IT4409/backend/controllers/EmailController.java
+++ backend/src/main/java/com/IT4409/backend/controllers/EmailController.java
@@ -3,6 +3,8 @@ package com.IT4409.backend.controllers;
 import com.IT4409.backend.entities.EmailDetails;
 import com.IT4409.backend.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +15,23 @@ public class EmailController {
     private EmailService emailService;
 
     @PostMapping("/sendMail")
-    public String
-    sendMail(@RequestBody EmailDetails details) {
-        String status = emailService.sendSimpleMail(details);
-        return status;
+    public ResponseEntity<?> sendMail(@RequestBody EmailDetails details) {
+        try{
+            String status = emailService.sendSimpleMail(details);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     // Sending email with attachment
     @PostMapping("/sendMailWithAttachment")
-    public String sendMailWithAttachment(@RequestBody EmailDetails details) {
-        String status = emailService.sendMailWithAttachment(details);
-        return status;
+    public ResponseEntity<?> sendMailWithAttachment(@RequestBody EmailDetails details) {
+        try{
+            String status = emailService.sendMailWithAttachment(details);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
