0fe047dcc37751e2d8eec46512bfcfd1eb9cc22e
Quan Dao Anh <anhquan7303qqq@gmail.com>
2024-05-14 10:39:21 +0700

    API Thêm sản phẩm

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -7,7 +7,9 @@ const axiosClient = axios.create({
     baseURL: API_BASE_URL,
 });
 
-const token = localStorage.getItem('jwt');
+const token = 'eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3MTU2NTYxMjgsImV4cCI6MTcxNTc0MjUyOCwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20ifQ.xzaxy9uT538OUaYAMXcnJUCHmvWOA2rU2EiPGu038zTwFnI4C0340vckwFod5-x7fk-eKCXSY8W8bu-WEB53qQ';
+// token
+
 
 axiosClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
